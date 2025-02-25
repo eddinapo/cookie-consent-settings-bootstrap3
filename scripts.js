6af956fe0ea3adcf68f708147080fe69d998bff6
eddinapo <31517542+eddinapo@users.noreply.github.com>
2025-02-25 02:14:08 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    if (!getCookie("cookieConsent")) {
+        $("#cookieConsent").modal('show');
+    }
+});
+
+function saveCookieSettings() {
+    var analyticsCookies = $("#analyticsCookies").is(":checked");
+    var marketingCookies = $("#marketingCookies").is(":checked");
+
+    setCookie("analyticsCookies", analyticsCookies, 365);
+    setCookie("marketingCookies", marketingCookies, 365);
+    setCookie("cookieConsent", true, 365);
+
+    $("#cookieConsent").modal('hide');
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
